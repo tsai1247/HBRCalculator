06f7d119af943a55adf4077b1396a24650fa7949
tsai1247 <jeff29cc91079@gmail.com>
2023-10-17 17:16:03 +0800

    [test] try using svg instead of webp

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,13 +9,19 @@
       :characters="team"
       v-on:clicked="showSetTeamDialog(true)"
     ></team-view>
+    <img :src="kayamori">
+    <img :src="izumiyuki">
+    123456
+    <v-img :src="kayamori"></v-img>
+    <v-img :src="izumiyuki"></v-img>
   </div>
 </template>
 
 <script setup>
 import { ref } from 'vue'
 import characterList from '@/common/character.json'
-
+import kayamori from '../assets/icon/kayamori.svg';
+import izumiyuki from '../assets/icon/izumiyuki.webp';
 import teamView from '@/components/teamView.vue';
 import setTeam from '@/components/setTeam.vue';
 const team = ref([characterList[0]]);
