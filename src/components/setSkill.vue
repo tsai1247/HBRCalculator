3a6eb8689397d4c7e4054b07fe0345144feb1eed
tsai1247 <jeff29cc91079@gmail.com>
2023-11-08 21:10:26 +0800

    [Modify] 技能選擇次數顯示優化

--- src/components/setSkill.vue
+++ src/components/setSkill.vue
@@ -6,40 +6,43 @@
       :items="skillList"
     >
       <template v-slot:default="{ item }">
-        <v-row class="ma-2">
-          <v-col cols="1">
-            <div>{{ item.count ?? 0 }}</div>
-          </v-col>
-          <v-col>
+        <div class="ma-2">
+          <v-tooltip
+            :text="`${t(item.description)}  已選擇 ${item.count ?? 0} 次`"
+            class="tooltip"
+          >
+            <template v-slot:activator="{ props }">
+              <v-btn
+                v-bind="props"
+                @click="selectSkill(item.name)"
+                @contextmenu.prevent="selectSkill(item.name, -1)"
+                rounded
+                text
+                height="100%"
+              >
+                <div class="text-left">
+                  <span class="text-h6 font-weight-bold">
+                    {{$t(item.name)}}
+                    <span
+                      v-for="n in item.count"
+                      :key="n"
+                      class="ml-1 purple"
+                    >●</span>
+                  </span>
+                  <v-spacer />
+                  <v-chip
+                    v-for="tag in item.tags"
+                    :key="tag"
+                    class="ma-2"
+                    color="purple"
+                    text-color="white"
+                  > {{ $t(tag) }} </v-chip>
+                </div>
 
-            <v-tooltip :text="$t(item.description)">
-              <template v-slot:activator="{ props }">
-                <v-btn
-                  v-bind="props"
-                  @click="selectSkill(item.name)"
-                  @contextmenu.prevent="selectSkill(item.name, -1)"
-                  rounded
-                  text
-                  height="100%"
-                >
-                  <div class="text-left">
-                    <span class="text-h6 font-weight-bold">{{$t(item.name)}}</span>
-                    <v-spacer />
-
-                    <v-chip
-                      v-for="tag in item.tags"
-                      :key="tag"
-                      class="ma-2"
-                      color="purple"
-                      text-color="white"
-                    > {{ $t(tag) }} </v-chip>
-                  </div>
-
-                </v-btn>
-              </template>
-            </v-tooltip>
-          </v-col>
-        </v-row>
+              </v-btn>
+            </template>
+          </v-tooltip>
+        </div>
       </template>
     </v-virtual-scroll>
   </div>
@@ -48,6 +51,9 @@
 <script setup>
 
 import { ref, computed, watch } from 'vue';
+import i18n from '@/plugins/i18n'
+const { t } = i18n.global
+
 import allSkillList from '@/common/skill.json'
 const props = defineProps({
   value: Boolean,
@@ -71,4 +77,11 @@ function selectSkill(name, step = 1) {
 
 </script>
 <style scoped>
+  .tooltip {
+    user-select: none;
+  }
+
+  .purple {
+    color: purple;
+  }
 </style>
