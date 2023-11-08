5afbe2c8f781d6b851fac6a37d31cdf1e2fc6e84
tsai1247 <jeff29cc91079@gmail.com>
2023-11-08 19:56:29 +0800

    [Add] skill tag，優化技能顯示版面

--- src/components/setSkill.vue
+++ src/components/setSkill.vue
@@ -1,24 +1,47 @@
 <template>
   <div v-if="props.value">
-
-    <v-row
-      class="ma-2"
-      v-for="{count, name, description} in skillList"
-      :key="name"
+    <v-virtual-scroll
+      height="100%"
+      width="100%"
+      :items="skillList"
     >
-      <v-col cols="1">
-        <div>{{ count ?? 0 }}</div>
-      </v-col>
-      <v-col cols="4">
-        <v-chip
-          @click="selectSkill(name)"
-          class="no-selectable"
-        >
-          <div>{{$t(name)}}</div>
-          <div>{{$t(description)}}</div>
-        </v-chip>
-      </v-col>
-    </v-row>
+      <template v-slot:default="{ item }">
+        <v-row class="ma-2">
+          <v-col cols="1">
+            <div>{{ item.count ?? 0 }}</div>
+          </v-col>
+          <v-col>
+
+            <v-tooltip :text="$t(item.description)">
+              <template v-slot:activator="{ props }">
+                <v-btn
+                  v-bind="props"
+                  @click="selectSkill(item.name)"
+                  @contextmenu.prevent="selectSkill(item.name, -1)"
+                  rounded
+                  text
+                  height="100%"
+                >
+                  <div class="text-left">
+                    <span class="text-h6 font-weight-bold">{{$t(item.name)}}</span>
+                    <v-spacer />
+
+                    <v-chip
+                      v-for="tag in item.tags"
+                      :key="tag"
+                      class="ma-2"
+                      color="purple"
+                      text-color="white"
+                    > {{ $t(tag) }} </v-chip>
+                  </div>
+
+                </v-btn>
+              </template>
+            </v-tooltip>
+          </v-col>
+        </v-row>
+      </template>
+    </v-virtual-scroll>
   </div>
 </template>
 
@@ -40,15 +63,12 @@ watch(() => props.characterId, () => {
   })[0]?.skills;
 })
 
-function selectSkill(name) {
+function selectSkill(name, step = 1) {
   const idx = skillList.value.findIndex(item => item.name === name);
   const count = skillList.value[idx].count ?? 0;
-  skillList.value[idx].count = (count + 1) % 3;
+  skillList.value[idx].count = (count + 3 + step) % 3;
 }
 
 </script>
 <style scoped>
-  .no-selectable {
-    user-select: none;
-  }
 </style>
